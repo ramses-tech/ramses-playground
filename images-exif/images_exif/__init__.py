827c67aac207c35e1a21d3f7e2f1f4e5b866c5d8
Artem Kostiuk <postatum@gmail.com>
2015-10-14 17:42:08 +0300

    Define basic schema and processors

--- images-exif/images_exif/__init__.py
+++ images-exif/images_exif/__init__.py
@@ -1,8 +1,31 @@
-import logging
-
 from pyramid.config import Configurator
+from ramses import registry
 
-log = logging.getLogger(__name__)
+
+@registry.add
+def validate_url_present(event):
+    if 'url' not in event.fields or not event.fields['url'].new_value:
+        raise KeyError('Missing required field: "url"')
+
+
+@registry.add
+def populate_name(event):
+    if 'name' not in event.fields:
+        url = event.fields['url'].new_value
+        full_name = url.strip().split('/')[-1]
+        name = full_name.split('.')[0]
+        event.set_field_value('name', name)
+
+
+@registry.add
+def readonly_url(event):
+    if event.view.request.action != 'create' and 'url' in event.fields:
+        raise Exception('Field "url" can not be changed')
+
+
+@registry.add
+def fill_exif(event):
+    return
 
 
 def main(global_config, **settings):
